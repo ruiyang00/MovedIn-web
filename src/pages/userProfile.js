6f0a20f2145754983cd0ef0d73358ab74bb47c9f
ZiqiYuanAngela <yuanziqi93@gmail.com>
2019-11-08 10:27:46 -0800

    userProfile.js update

--- src/pages/userProfile.js
+++ src/pages/userProfile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link,withRouter } from 'react-router-dom';
-import {Button, Form,Grid} from 'semantic-ui-react';
+import {Button, Form,Grid, Card} from 'semantic-ui-react';
 import * as ROUTES from "./../logistics/routes";
 import axios from 'axios';
 
@@ -10,10 +10,21 @@ export default class UserProfile extends Component{
     super(props);
     this.state={
       nameToDisplay:'',
-      city:''
+      city:'',
+      allUsers:[],
     };
     this.onSubmit=this.onSubmit.bind(this);
     this.handleInputChange= this.handleInputChange.bind(this);
+  }
+
+  componentDidMount=()=>{
+    axios.post('http://localhost:5000/users/homePage')
+    .then(function(response){
+         console.log(response.data.allusers);
+         this.setState({allUsers:response.data.allusers})
+
+ }.bind(this));
+
   }
 
 
@@ -35,7 +46,7 @@ export default class UserProfile extends Component{
     city: this.state.city
   }).
   then(function(response){
-    window.alert("Profile Completed!");
+    window.alert(response.data);
   });
 
 
@@ -45,50 +56,67 @@ export default class UserProfile extends Component{
 
 
   render(){
-  //  const {handleSubmit} = this.props;
-    return (
-<Grid centered columns={3}>
+   const {handleSubmit} = this.props;
+  //
+    const {allUsers}=this.state;
+    console.log(allUsers);
+      return(
+    allUsers.map((user)=>{
+
+      return(
+
+      <Card>
+        <Card.Content>
+           {user.city}
+        </Card.Content>
+      </Card>
+
+)
+
+// <Grid centered columns={3}>
+//
+//
+//
+//             <Form onSubmit={this.onSubmit}>
+//             <Form.Input
+//             icon="user"
+//             iconPosition="left"
+//             type="text"
+//             name='nameToDisplay'
+//             value={this.state.nameToDisplay}
+//             onChange={this.handleInputChange}
+//             required
+//             />
+//
+//
+//             <Form.Input
+//             icon="building"
+//             iconPosition="left"
+//             type="text"
+//             name='city'
+//             value={this.state.city}
+//             onChange={this.handleInputChange}
+//             required
+//             />
+//
+//             <Button
+//               color="pink"
+//               fluid
+//               size="large"
+//               type="submit">Submit
+//             </Button>
+//
+//             </Form>
+//         </Grid>
+//
 
 
 
-            <Form onSubmit={this.onSubmit}>
-            <Form.Input
-            icon="user"
-            iconPosition="left"
-            type="text"
-            name='nameToDisplay'
-            value={this.state.nameToDisplay}
-            onChange={this.handleInputChange}
-            required
-            />
-
-
-            <Form.Input
-            icon="building"
-            iconPosition="left"
-            type="text"
-            name='city'
-            value={this.state.city}
-            onChange={this.handleInputChange}
-            required
-            />
-
-            <Button
-              color="pink"
-              fluid
-              size="large"
-              type="submit">Submit
-            </Button>
-
-            </Form>
-        </Grid>
 
 
 
-
-
-
-    );
+})
+)
   }
 
 
